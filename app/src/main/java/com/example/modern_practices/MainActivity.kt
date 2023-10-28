03ce2d4e9949a3bfef9e5f74376dd9290cc0f2aa
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-28 15:13:58 +0530

    Applied navigate to previous page by BackHandler

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.modern_practices
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
@@ -38,29 +44,30 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebViewPage(url: String, modifier: Modifier) {
-    AndroidView(factory = {
-        WebView(it).apply {
-            webViewClient = WebViewClient()
 
-            settings.javaScriptEnabled = true
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView: WebView? = null
 
-            loadUrl(url)
+    AndroidView(
+        modifier = modifier,
+        factory = {
+            WebView(it).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
 
-            webViewClient = object: WebViewClient() {
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    removeElement(view)
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        backEnabled = view.canGoBack()
+                    }
                 }
 
-                private fun removeElement(view: WebView?) {
-                    //view?.loadUrl("javascript:(function() { document.getElementById('blog-pager').style.display='none';})()")
-
-                    view?.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[0].style.display='none';})()")
-                    view?.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[1].style.display='none';})()")
-                }
+                loadUrl(url)
+                webView = this
             }
-        }
-    }, update = { it.loadUrl(url) },
-        modifier = modifier
+        }, update = { view -> webView = view },
     )
+
+    BackHandler(enabled = backEnabled) {
+        webView?.goBack()
+    }
 }
