4b03378d96e724b88c8006d03852825d07f1ff95
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-28 16:49:37 +0530

    Removed or Stop Ad on webpage

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.modern_practices
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
@@ -17,8 +19,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.io.InputStreamReader
 
 class MainActivity : ComponentActivity() {
 
@@ -45,8 +52,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WebViewPage(url: String, modifier: Modifier) {
 
-    var backEnabled by remember { mutableStateOf(false) }
-    var webView: WebView? = null
+    val context = LocalContext.current
+    val adServers = StringBuilder()
+    var line: String
+    val inputStream = context.resources.openRawResource(R.raw.adblockserverlist)
+    val br = BufferedReader(InputStreamReader(inputStream))
+
+    try {
+        while (br.readLine().also { line = it } != null) {
+            adServers.append(line)
+            adServers.append("\n")
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
 
     AndroidView(
         modifier = modifier,
@@ -56,18 +75,22 @@ fun WebViewPage(url: String, modifier: Modifier) {
                 settings.javaScriptEnabled = true
 
                 webViewClient = object : WebViewClient() {
-                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
-                        backEnabled = view.canGoBack()
+                    override fun shouldInterceptRequest(
+                        view: WebView?, request: WebResourceRequest?
+                    ): WebResourceResponse? {
+
+                        val empty = ByteArrayInputStream("".toByteArray())
+                        val kk5 = adServers.toString()
+                        if (kk5.contains(":::::" + request?.url?.host))
+                            return WebResourceResponse("text/plain", "utf-8", empty)
+
+                        return super.shouldInterceptRequest(view, request)
                     }
                 }
 
                 loadUrl(url)
-                webView = this
             }
-        }, update = { view -> webView = view },
+        },
+        update = { view -> view.loadUrl(url) },
     )
-
-    BackHandler(enabled = backEnabled) {
-        webView?.goBack()
-    }
 }
