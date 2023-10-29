54769eeb20a82d6ba6abd488277e3aee306d7750
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-29 16:13:37 +0530

    Transferred back to the template for new practices

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -1,29 +1,13 @@
 package com.example.modern_practices
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import android.os.Bundle
-import android.view.ViewGroup
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
-
-private var webpageURL = "https://www.boltuix.com/"
 
 class MainActivity : ComponentActivity() {
 
@@ -35,72 +19,8 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    WebViewPage(url = webpageURL)
-                }
+                ) {}
             }
         }
     }
-}
-
-@SuppressLint("SetJavaScriptEnabled")
-@Composable
-fun WebViewPage(url: String) {
-
-    val mutableStateTrigger = remember { mutableStateOf(false) }
-
-    AndroidView(
-        factory = {
-            WebView(it).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                webViewClient = WebViewClient()
-                settings.javaScriptEnabled = true
-
-                webViewClient = object : WebViewClient() {
-                    override fun onReceivedError(
-                        view: WebView?,
-                        request: WebResourceRequest?,
-                        error: WebResourceError?
-                    ) {
-                        super.onReceivedError(view, request, error)
-                        webpageURL =
-                            if (isOnline(context)) "file:///android_asset/404.html"
-                            else "file:///android_asset/error.html"
-                        mutableStateTrigger.value = true
-                    }
-                }
-
-                loadUrl(url)
-            }
-        },
-        update = { view -> view.loadUrl(url) },
-    )
-    if (mutableStateTrigger.value)
-        WebViewPage(url = webpageURL)
-}
-
-fun isOnline(context: Context): Boolean {
-    val connectivityManager =
-        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-                ?: return false
-
-        return when {
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            else -> false
-        }
-    } else {
-        @Suppress("DEPRECATION")
-        if (connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting)
-            return true
-    }
-    return false
 }
