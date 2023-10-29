f3f9fd38015f408b83e7967535c1bd06b6df869c
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-29 11:01:19 +0530

    Displayed Error page if URL is invalid

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.modern_practices
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
@@ -12,14 +13,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
-import java.io.BufferedReader
-import java.io.ByteArrayInputStream
-import java.io.IOException
-import java.io.InputStreamReader
+
+private var webpageURL = "https://www.harsh1471014.com/"
 
 class MainActivity : ComponentActivity() {
 
@@ -32,10 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WebViewPage(
-                        url = "https://www.boltuix.com/",
-                        modifier = Modifier.fillMaxSize()
-                    )
+                    WebViewPage(url = webpageURL)
                 }
             }
         }
@@ -44,41 +41,29 @@ class MainActivity : ComponentActivity() {
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun WebViewPage(url: String, modifier: Modifier) {
+fun WebViewPage(url: String) {
 
-    val context = LocalContext.current
-    val adServers = StringBuilder()
-    var line: String?
-    val inputStream = context.resources.openRawResource(R.raw.adblockserverlist)
-    val br = BufferedReader(InputStreamReader(inputStream))
-
-    try {
-        while (br.readLine().also { line = it } != null) {
-            adServers.append(line)
-            adServers.append("\n")
-        }
-    } catch (e: IOException) {
-        e.printStackTrace()
-    }
+    val mutableStateTrigger = remember { mutableStateOf(false) }
 
     AndroidView(
-        modifier = modifier,
         factory = {
             WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
                 webViewClient = WebViewClient()
                 settings.javaScriptEnabled = true
 
                 webViewClient = object : WebViewClient() {
-                    override fun shouldInterceptRequest(
-                        view: WebView?, request: WebResourceRequest?
-                    ): WebResourceResponse? {
-
-                        val empty = ByteArrayInputStream("".toByteArray())
-                        val kk5 = adServers.toString()
-                        if (kk5.contains(":::::" + request?.url?.host))
-                            return WebResourceResponse("text/plain", "utf-8", empty)
-
-                        return super.shouldInterceptRequest(view, request)
+                    override fun onReceivedError(
+                        view: WebView?,
+                        request: WebResourceRequest?,
+                        error: WebResourceError?
+                    ) {
+                        super.onReceivedError(view, request, error)
+                        webpageURL = "file:///android_asset/404.html"
+                        mutableStateTrigger.value = true
                     }
                 }
 
@@ -87,4 +72,6 @@ fun WebViewPage(url: String, modifier: Modifier) {
         },
         update = { view -> view.loadUrl(url) },
     )
+    if (mutableStateTrigger.value)
+        WebViewPage(url = webpageURL)
 }
