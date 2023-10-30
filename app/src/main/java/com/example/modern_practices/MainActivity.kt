b65436eb462ba2dd42c248e2eaa4c3aad9331781
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-30 19:18:54 +0530

    Designed NoMessage screen

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.modern_practices.screens.NoChat
 import com.example.modern_practices.screens.NoInternet
+import com.example.modern_practices.screens.NoMessage
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoInternet()
+                    NoMessage()
                 }
             }
         }
