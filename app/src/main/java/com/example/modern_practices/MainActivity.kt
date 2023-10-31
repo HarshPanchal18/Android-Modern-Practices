4c896e0968c5e97847d0fb73bbb351ad1c17e485
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-31 19:09:38 +0530

    Designed NoChat screen

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.modern_practices.screens.NoChat
+import com.example.modern_practices.screens.NoInternet
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoChat()
+                    NoInternet()
                 }
             }
         }
