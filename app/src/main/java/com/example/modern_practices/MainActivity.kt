6be307ca76564a20fc5d7df36379b316d97e1629
Harsh Panchal <harshhhh1803@gmail.com>
2023-11-02 10:29:06 +0530

    Designed NoResultSearch screen

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.modern_practices.screens.NoResult
+import com.example.modern_practices.screens.NoResultSearch
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoResult()
+                    NoResultSearch()
                 }
             }
         }
