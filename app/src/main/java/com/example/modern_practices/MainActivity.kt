59c7e3eee982509223e3a23c93927a6644f0e73a
Harsh Panchal <harshhhh1803@gmail.com>
2023-11-05 18:58:04 +0530

    Designed PaymentFailed screen

--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.modern_practices.screens.NotificationPermission
-import com.example.modern_practices.screens.NotificationUnRead
-import com.example.modern_practices.screens.OhNo
+import com.example.modern_practices.screens.PaymentFailed
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    OhNo()
+                    PaymentFailed()
                 }
             }
         }
