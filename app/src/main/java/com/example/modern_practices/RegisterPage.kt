3c79589fe262c96739de81b8284e287da3acd2d6
Harsh Panchal <harshhhh1803@gmail.com>
2023-11-10 12:18:17 +0530

    Re-added files which was deleted by mistake

---
+++ app/src/main/java/com/example/modern_practices/RegisterPage.kt
@@ -0,0 +1,350 @@
+package com.example.modern_practices
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun RegisterPage(navController: NavController) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Transparent)
+    ) {
+        Box(
+            modifier = Modifier
+                /*.background(
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    shape = RoundedCornerShape(25.dp, 5.dp, 25.dp, 5.dp)
+                )*/
+                .align(Alignment.BottomCenter)
+        ) {
+
+            Image(
+                painterResource(id = R.drawable.user_reg),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Text(
+                    text = "Create An Account",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 130.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                RegisterName()
+
+                Spacer(modifier = Modifier.padding(3.dp))
+                RegisterPhone()
+
+                Spacer(modifier = Modifier.padding(3.dp))
+                RegisterEmail()
+
+                Spacer(modifier = Modifier.padding(3.dp))
+                RegisterPassword()
+
+                Spacer(modifier = Modifier.padding(3.dp))
+                RegisterPasswordConfirm()
+
+                val gradientColor = listOf(Color(0xFF484BF1), Color(0xFF673AB7))
+                val cornerRadius = 16.dp
+
+
+                Spacer(modifier = Modifier.padding(10.dp))
+                GradientButton(
+                    gradientColors = gradientColor,
+                    cornerRadius = cornerRadius,
+                    nameButton = "Create An Account",
+                    roundedCornerShape = RoundedCornerShape(topStart = 30.dp, bottomEnd = 30.dp)
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+                TextButton(onClick = {
+                    navController.navigate(route = "loginPage") {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                }) {
+                    Text(
+                        text = "Sign In",
+                        letterSpacing = 1.sp,
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(10.dp))
+                TextButton(onClick = {
+                    navController.navigate(route = "resetPage") {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                }) {
+                    Text(
+                        text = "Reset password",
+                        letterSpacing = 1.sp,
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterName() {
+    val focusManager = LocalFocusManager.current
+    var text by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(value = text, onValueChange = { text = it },
+        shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp),
+        label = {
+            Text(
+                text = "Name",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.labelMedium
+            )
+        },
+        placeholder = { Text(text = "Name") },
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Text
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = MaterialTheme.colorScheme.primary
+        ),
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(0.8f),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Next)
+            }
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterEmail() {
+    val focusManager = LocalFocusManager.current
+    var text by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(value = text, onValueChange = { text = it },
+        shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp),
+        label = {
+            Text(
+                text = "Email",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.labelMedium
+            )
+        },
+        placeholder = { Text(text = "Email") },
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Email
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = MaterialTheme.colorScheme.primary
+        ),
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(0.8f),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Next)
+                // do something here
+            }
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterPhone() {
+    val focusManager = LocalFocusManager.current
+    var text by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(value = text, onValueChange = { text = it },
+        shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp),
+        label = {
+            Text(
+                text = "Phone",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.labelMedium
+            )
+        },
+        placeholder = { Text(text = "Phone") },
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Phone
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = MaterialTheme.colorScheme.primary
+        ),
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(0.8f),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Next)
+                // do something here
+            }
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterPassword() {
+    val focusManager = LocalFocusManager.current
+    var text by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+
+    OutlinedTextField(value = text, onValueChange = { text = it },
+        shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp),
+        label = {
+            Text(
+                text = "Password",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.labelMedium
+            )
+        },
+        placeholder = { Text(text = "Password") },
+        visualTransformation =
+        if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Password
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = MaterialTheme.colorScheme.primary
+        ),
+        trailingIcon = {
+            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                val visibilityIcon = if (passwordHidden) Visibility else VisibilityOff
+                // Please provide localized description for accessibility services
+                val description = if (passwordHidden) "Show password" else "Hide password"
+                Icon(imageVector = visibilityIcon, contentDescription = description)
+            }
+        },
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(0.8f),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Next)
+                // do something here
+            }
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun RegisterPasswordConfirm() {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp),
+        label = {
+            Text(
+                "Confirm Password",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.labelMedium,
+            )
+        },
+        visualTransformation =
+        if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = MaterialTheme.colorScheme.primary
+        ),
+        trailingIcon = {
+            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                val visibilityIcon =
+                    if (passwordHidden) Visibility else VisibilityOff
+                // Please provide localized description for accessibility services
+                val description = if (passwordHidden) "Show password" else "Hide password"
+                Icon(imageVector = visibilityIcon, contentDescription = description)
+            }
+        },
+        modifier = Modifier.fillMaxWidth(0.8f),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.clearFocus()
+                keyboardController?.hide()
+                // do something here
+            }
+        )
+    )
+}
