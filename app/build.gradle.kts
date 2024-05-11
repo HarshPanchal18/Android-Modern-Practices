e315c190d9774cec5a65a4c5ff558ba251e00ee2
Harsh Panchal <harshhhh1803@gmail.com>
2024-05-11 13:36:45 +0530

    Added WorkManager model to show notigfication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -69,22 +69,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
     //implementation("androidx.compose.material:material")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
 
-    //implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    val voyagerVersion = "1.0.0"
-    implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion") // Navigator
-    implementation("cafe.adriel.voyager:voyager-bottom-sheet-navigator:$voyagerVersion") // BottomSheetNavigator
-    implementation("cafe.adriel.voyager:voyager-tab-navigator:$voyagerVersion") // TabNavigator
-    implementation("cafe.adriel.voyager:voyager-transitions:$voyagerVersion") // Transitions
-
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
--- app/src/main/java/com/example/modern_practices/MainActivity.kt
+++ app/src/main/java/com/example/modern_practices/MainActivity.kt
@@ -1,40 +1,20 @@
 package com.example.modern_practices
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import cafe.adriel.voyager.core.stack.rememberStateStack
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.modern_practices.ui.theme.ModernPracticesTheme
-import java.util.UUID
 
 class MainActivity : ComponentActivity() {
-
-    private val randomValue: String
-        get() = UUID.randomUUID().toString().substringBefore('-')
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,115 +25,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val stateStack = rememberStateStack<String>()
-                    val (selectedItem, selectItem) = rememberSaveable { mutableStateOf("") }
-
                     Column {
-                        LazyColumn(
-                            contentPadding = PaddingValues(16.dp),
-                            reverseLayout = true,
-                            modifier = Modifier.weight(.8f)
-                        ) {
-                            itemsIndexed(stateStack.items) { index, item ->
-                                ListItem(
-                                    index = index,
-                                    item = item,
-                                    isLast = stateStack.lastItemOrNull == item,
-                                    isSelected = selectedItem == item,
-                                    onSelected = selectItem
-                                )
-                            }
-
-                            if (stateStack.isEmpty) {
-                                item {
-                                    Text(
-                                        text = "EMPTY",
-                                        textAlign = TextAlign.Center,
-                                        color = Color.DarkGray,
-                                        fontWeight = FontWeight.Normal,
-                                        fontFamily = FontFamily.Monospace,
-                                        modifier = Modifier.fillMaxWidth()
-                                    )
-                                }
-                            }
-
-                        } // LazyColumn
-
-                        Row(modifier = Modifier.weight(0.1F)) {
-                            ActionButton(text = "Pop", enabled = stateStack.canPop) {
-                                if (stateStack.lastItemOrNull == selectedItem)
-                                    selectItem("")
-
-                                stateStack.pop()
-                            }
-
-                            ActionButton(text = "Pop until", enabled = stateStack.canPop) {
-                                if (selectedItem.isBlank()) {
-                                    Toast.makeText(
-                                        this@MainActivity,
-                                        "Select an item first",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                    return@ActionButton
-                                }
-
-                                selectItem("")
-                                stateStack.popUntil { it == selectedItem }
-                            }
-
-                            ActionButton(text = "Push") {
-                                stateStack.push(randomValue)
-                            }
+                        Button(onClick = {
+                            val workRequest = OneTimeWorkRequestBuilder<MyWorker>().build()
+                            WorkManager.getInstance(applicationContext).enqueue(workRequest)
+                        }) {
+                            Text(text = "Show notification")
                         }
-
-                        Row(modifier = Modifier.weight(0.1F)) {
-                            ActionButton(text = "Replace") {
-                                stateStack.replace(randomValue)
-                            }
-
-                            ActionButton(text = "ReplaceAll") {
-                                stateStack.replaceAll(randomValue)
-                            }
-                        }
-
-                    } // Column
-                } // Surface
+                    }
+                }
             }
         }
     }
 
-    @Composable
-    fun ListItem(
-        index: Int,
-        item: String,
-        isLast: Boolean,
-        isSelected: Boolean,
-        onSelected: (String) -> Unit,
-    ) {
-        val formattedIndex = index.toString().padStart(3, ' ')
-        Text(
-            text = "$formattedIndex: $item",
-            color = if (isSelected) Color.Red else Color.Black,
-            fontWeight = if (isLast) FontWeight.Bold else FontWeight.Normal,
-            fontFamily = FontFamily.Monospace,
-            modifier = Modifier.clickable { onSelected(item) }
-        )
-    }
-
-    @Composable
-    fun RowScope.ActionButton(
-        text: String,
-        enabled: Boolean = true,
-        onClick: () -> Unit,
-    ) {
-        Button(
-            onClick = onClick,
-            enabled = enabled,
-            modifier = Modifier
-                .weight(.1f)
-                .padding(8.dp)
-        ) {
-            Text(text = text)
-        }
-    }
 }
